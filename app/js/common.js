733fe037c753e3fceea8bb949fe4bfbc96b9779c
Dmitry <sheklein.dima@gmail.com>
2017-08-10 14:39:12 +0300

    Add block Burger/Team

--- app/js/common.js
+++ app/js/common.js
@@ -4,4 +4,32 @@ $(function() {
 	$("#fullpage").fullpage({
 		verticalCentered: false
 	})
+
+	// Slider
+ 	$('.slider__wrapper').owlCarousel({
+		dots : false,
+		nav:false,
+		slideSpeed : 800,
+		paginationSpeed : 400,
+		singleItem: true,
+		items: 1
+	})
+	var slider = $('.slider__wrapper');
+	$('.slider__arrow--left').on('click', function(e) {
+		e.preventDefault();
+		slider.trigger('prev.owl.carousel');
+	});	
+	$('.slider__arrow--right').on('click', function(e) {
+		e.preventDefault();
+		slider.trigger('next.owl.carousel');
+	});	
+	//Accordeon
+	$('.team__accord .team__accord-link').on('click', function(){
+		var $this = $(this),
+			content = $this.next();
+		$('.team__accord .team__accord-info:visible').not(content).slideUp(300)
+		$('.team__accord .team__accord-link').not($this).removeClass('active')
+		$this.toggleClass('active')
+		content.slideToggle(300)
+	})
 });
